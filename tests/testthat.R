0d58a234f79a3dc1ce6e7eca3b5263a4ed959f17
Duncan Garmonsway <nacnudus@gmail.com>
2019-01-28 15:48:01 +0000

    Migrate tests to vdiffr from visualTest

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,6 @@
 library(testthat)
 library(govstyle)
 library(ggplot2)
-library(visualTest)
 # Install visualTest to allow checking against a reference plot.
 #devtools::install_github("MangoTheCat/visualTest")
 
--- tests/testthat/test_theme_gov_complex.R
+++ tests/testthat/test_theme_gov_complex.R
@@ -1,90 +1,31 @@
 context('theme_gov() works with complex examples')
 
 test_that(
-  'theme_gov can replicate a rug and point plot',
+  'theme_gov can replicate a complex rug and point plot',
   {
-    
-    # Check whether comparison file exists, and delete if so
-    
-    test_png <- file.path('figures', 'test_rugpoint_test.png')
-    ref_png <- file.path('figures', 'test_rugpoint_reference.png')
-    
-    if (file.exists(test_png)) file.remove(test_png)
-    
-    test_that(
-      'test_png does not already exist',
-      {
-        expect_false(file.exists(test_png))
-      }
-    )
-    
-    # Create a new figure as png
-    
-    p <- ggplot2::ggplot(mtcars) + 
+    p <- ggplot2::ggplot(mtcars) +
       ggplot2::aes_(
-        ~wt, 
-        ~mpg, 
-        colour = ~factor(cyl), 
-        group = ~cyl) + 
+        ~wt,
+        ~mpg,
+        colour = ~factor(cyl),
+        group = ~cyl) +
       ggplot2::geom_rug() +
-      ggplot2::geom_point() + 
-      govstyle::theme_gov(base_colour = 'black') + 
+      ggplot2::geom_point() +
+      govstyle::theme_gov(base_colour = 'black') +
       ggplot2::scale_colour_manual(
         values = unname(govstyle::gov_cols[c('red', 'purple','green')])
-      ) + 
-      ggplot2::theme(legend.position='bottom') + 
+      ) +
+      ggplot2::theme(legend.position='bottom') +
       ggplot2::ylab('Miles per gallon (mpg)') +
       ggplot2::ylab('Weight (1000 lbs)')
-    
-    ggplot2::ggsave(
-      width = 5,
-      height = 5,
-      filename = test_png,
-      plot = p
-    )
-    
-    # Check that the new file was created
-    
-    test_that(
-      'test_png was created',
-      {
-        expect_true(file.exists(test_png))
-      }
-    )
-    
-    # Compare new figure with reference
-    
-    visualTest::isSimilar(
-      file = ref_png,
-      fingerprint = visualTest::getFingerprint(
-        file = test_png
-      ),
-      threshold = 1e-3
-    )
-    
+
+    vdiffr::expect_doppelganger("complex rug and point plot", p)
   }
 )
 
 test_that(
   'theme_gov can replicate a geom_path plot',
   {
-    
-    # Check whether comparison file exists, and delete if so
-    
-    test_png <- file.path('figures', 'test_path_test.png')
-    ref_png <- file.path('figures', 'test_path_reference.png')
-    
-    if (file.exists(test_png)) file.remove(test_png)
-    
-    test_that(
-      'test_png does not already exist',
-      {
-        expect_false(file.exists(test_png))
-      }
-    )
-    
-    # Create a new figure as png
-    
     p <- ggplot2::ggplot(GVA) +
       ggplot2::aes(
         y = GVA,
@@ -93,32 +34,7 @@ test_that(
       ) +
       ggplot2::geom_path() +
       govstyle::theme_gov()
-    
-    ggplot2::ggsave(
-      width = 5,
-      height = 5,
-      filename = test_png,
-      plot = p
-    )
-    
-    # Check that the new file was created
-    
-    test_that(
-      'test_png was created',
-      {
-        expect_true(file.exists(test_png))
-      }
-    )
-    
-    # Compare new figure with reference
-    
-    visualTest::isSimilar(
-      file = ref_png,
-      fingerprint = visualTest::getFingerprint(
-        file = test_png
-      ),
-      threshold = 1e-3
-    )
-    
+
+    vdiffr::expect_doppelganger("geom_path plot", p)
   }
 )
