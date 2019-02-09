8e6a404c3eca104e4e7ea9ed5221505a2bdb2d36
mattkerlogue <matt.kerlogue@cabinetoffice.gov.uk>
2019-02-09 18:37:29 +0000

    Update visual tests to vdiffr

--- tests/testthat/test-scale_gov.R
+++ tests/testthat/test-scale_gov.R
@@ -8,21 +8,6 @@ df <- data.frame(
 test_that(
     "scale_gov colours column chart",
     {
-        # Check whether comparison file exists, and delete if so
-    
-    test_png <- file.path('figures', 'test-scale_gov-col-test.png')
-    ref_png <- file.path('figures', 'test-scale_gov-col-reference.png')
-    
-    if (file.exists(test_png)) file.remove(test_png)
-    
-    test_that(
-        'test_png does not already exist',
-        {
-            expect_false(file.exists(test_png))
-        }
-    )
-    
-    # Create a new figure as png
     
     p <- ggplot(df, aes(x = year, y = price, fill = group)) +
         geom_col(position = "dodge") +
@@ -31,50 +16,14 @@ test_that(
         scale_x_continuous(breaks = c(2009, 2010, 2011)) +
         ylim(0,40)
     
-    ggplot2::ggsave(
-        width = 5,
-        height = 5,
-        filename = test_png,
-        plot = p
-    )
-    
-    # Check that the new file was created
-    
-    test_that(
-        'test_png was created',
-        {
-            expect_true(file.exists(test_png))
-        }
-    )
-    
-    visualTest::isSimilar(
-        file = ref_png,
-        fingerprint = visualTest::getFingerprint(
-            file = test_png
-        ),
-        threshold = 1e-3
-    )
+    vdiffr::expect_doppelganger("scale gov column", p)
+
     }
 )
 
 test_that(
     "scale_gov colours line chart",
     {
-        # Check whether comparison file exists, and delete if so
-        
-        test_png <- file.path('figures', 'test-scale_gov-line-test.png')
-        ref_png <- file.path('figures', 'test-scale_gov-line-reference.png')
-        
-        if (file.exists(test_png)) file.remove(test_png)
-        
-        test_that(
-            'test_png does not already exist',
-            {
-                expect_false(file.exists(test_png))
-            }
-        )
-        
-        # Create a new figure as png
         
         p <- ggplot(df, aes(x = year, y = price, colour = group)) +
             geom_line(size = 1.5) +
@@ -83,50 +32,13 @@ test_that(
             scale_x_continuous(breaks = c(2009, 2010, 2011)) +
             ylim(0,40)
         
-        ggplot2::ggsave(
-            width = 5,
-            height = 5,
-            filename = test_png,
-            plot = p
-        )
-        
-        # Check that the new file was created
-        
-        test_that(
-            'test_png was created',
-            {
-                expect_true(file.exists(test_png))
-            }
-        )
-        
-        visualTest::isSimilar(
-            file = ref_png,
-            fingerprint = visualTest::getFingerprint(
-                file = test_png
-            ),
-            threshold = 1e-3
-        )
+        vdiffr::expect_doppelganger("scale gov line", p)
     }
 )
 
 test_that(
     "scale_gov colours point + line chart",
     {
-        # Check whether comparison file exists, and delete if so
-        
-        test_png <- file.path('figures', 'test-scale_gov-point-test.png')
-        ref_png <- file.path('figures', 'test-scale_gov-point-reference.png')
-        
-        if (file.exists(test_png)) file.remove(test_png)
-        
-        test_that(
-            'test_png does not already exist',
-            {
-                expect_false(file.exists(test_png))
-            }
-        )
-        
-        # Create a new figure as png
         
         p <- ggplot(df, aes(x = year, y = price, colour = group)) +
             geom_line(size = 1.5) +
@@ -136,28 +48,7 @@ test_that(
             scale_x_continuous(breaks = c(2009, 2010, 2011)) +
             ylim(0,40)
         
-        ggplot2::ggsave(
-            width = 5,
-            height = 5,
-            filename = test_png,
-            plot = p
-        )
-        
-        # Check that the new file was created
-        
-        test_that(
-            'test_png was created',
-            {
-                expect_true(file.exists(test_png))
-            }
-        )
-        
-        visualTest::isSimilar(
-            file = ref_png,
-            fingerprint = visualTest::getFingerprint(
-                file = test_png
-            ),
-            threshold = 1e-3
-        )
+        vdiffr::expect_doppelganger("scale gov points", p)
+
     }
 )
